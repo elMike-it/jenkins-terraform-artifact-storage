c46b4191a31ada34eab94fdeeed4f1528d756b47
elMike-it <miguel.elizondo@trend-it.com.mx>
2025-05-05 15:10:35 -0600

    Test 7

--- terraform/pipeline-dev/artifact.tf
+++ terraform/pipeline-dev/artifact.tf
@@ -1,7 +1,7 @@
 module "artifact" {
   source         = "../modules/artifact"
   location       = "us-west1"
-  repository_id  = "mike-repository-dev-test6"
+  repository_id  = "mike-repository-dev-test7"
   description    = "example docker repository"
   format         = "DOCKER"
   immutable_tags = true
