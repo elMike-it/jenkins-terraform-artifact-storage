1f27cf53d572f6151237b1570d051cd5347f1fa8
elMike-it <miguel.elizondo@trend-it.com.mx>
2025-05-13 11:53:03 -0600

    Ft-Name-Changed

--- terraform/pipeline-pro/artifact.tf
+++ terraform/pipeline-pro/artifact.tf
@@ -1,7 +1,7 @@
 module "artifact" {
   source         = "../modules/artifact"
   location       = "us-west1"
-  repository_id  = "mike-repository-pro-test11"
+  repository_id  = "mike-repository-pro-test12"
   description    = "example docker repository"
   format         = "DOCKER"
   immutable_tags = true
--- terraform/pipeline-qas/artifact.tf
+++ terraform/pipeline-qas/artifact.tf
@@ -1,7 +1,7 @@
 module "artifact" {
   source         = "../modules/artifact"
   location       = "us-west1"
-  repository_id  = "mike-repository-qas-test11"
+  repository_id  = "mike-repository-qas-test12"
   description    = "example docker repository"
   format         = "DOCKER"
   immutable_tags = true
