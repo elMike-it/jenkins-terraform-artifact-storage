329aabe42c994eaba3217c27477bb1710a8f210d
elMike-it <miguel.elizondo@trend-it.com.mx>
2025-06-10 20:24:35 -0600

    Backend prueba

--- terraform/pipeline-dev/backend.tf
+++ terraform/pipeline-dev/backend.tf
@@ -1,6 +1,6 @@
-# terraform {
-#   backend "gcs" {
-#     bucket = "mike-back"
-#     prefix = "tf-state"
-#   }
-# }
+terraform {
+  backend "gcs" {
+    bucket = "mike-back"
+    prefix = "tf-state"
+  }
+}
